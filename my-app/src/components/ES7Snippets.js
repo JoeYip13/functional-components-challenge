41961eb9ad2c4a1a661fdd536c379f7114649ef0
JoeYip13 <>
2023-09-19 11:52:18 +0000

    feat(components): add ES7 snippets

---
+++ my-app/src/components/ES7Snippets.js
@@ -0,0 +1,59 @@
+// rfce
+// import React from 'react'
+
+// function ES7Snippets() {
+//   return (
+//     <div>ES7Snippets</div>
+//   )
+// }
+
+// export default ES7Snippets
+
+
+
+// rafce arrow function
+// import React from 'react'
+
+// const ES7Snippets = () => {
+//   return (
+//     <div>ES7Snippets</div>
+//   )
+// }
+
+// export default ES7Snippets
+
+
+
+// rce class component
+// import React, { Component } from 'react'
+
+// export class ES7Snippets extends Component {
+//   render() {
+//     return (
+//       <div>ES7Snippets</div>
+//     )
+//   }
+// }
+
+// export default ES7Snippets
+
+
+
+// rconst contructor
+// constructor(props) {
+//   super(props)
+
+//   this.state = {
+//      first
+//   }
+// }
+
+
+
+// clg console log
+// console.log(first)
+
+
+
+// clo console log object
+// console.log('first', first)
