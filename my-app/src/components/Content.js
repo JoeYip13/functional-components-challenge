38782cdef3ff3b71bfb220c7f6fc90048143592e
JoeYip13 <>
2023-09-23 12:42:06 +0000

    feat(components): search feature challenge

--- my-app/src/components/Content.js
+++ my-app/src/components/Content.js
@@ -10,20 +10,48 @@ export class Content extends Component {
     
       this.state = {
         isLoaded: false,
+        posts: [],
       }
     }
     componentDidMount() {
         setTimeout(() => {
             this.setState({
                 isLoaded: true,
+                posts: savedPosts,
             })
         }, 2000)
     }
+
+    handleSearchInput = (event) => {
+        // const name takes the input from the form event & converts to lower case if user search in capital letters 
+        const name = event.target.value.toLowerCase()
+        console.log(name)
+        // const filteredPost filters the post name by lower case and includes which compares the input value to the post
+        const filteredPost = savedPosts.filter((post) => {
+            return post.name.toLowerCase().includes(name)
+        })
+        // set new state of post to filterPost
+        this.setState({
+            posts: filteredPost
+        })
+    }
+
     render() {
         return (
         <div className={css.Content}>
             <div className={css.TitleBar}>
                 <h1>My Photos</h1>
+                <form>
+                    <label htmlFor="searchInput">Search:</label>
+                    <input
+                        type="search"
+                        id="searchInput"
+                        placeholder="By Author"
+                        onChange={(event) => this.handleSearchInput(event)}
+                    />
+                    {/* shows the length array of the posts */}
+                    <h4>posts found: {this.state.posts.length}</h4>
+                </form>
             </div>
             <div className={css.SearchResults}>
                 
@@ -42,7 +70,8 @@ export class Content extends Component {
                     // pass savedPost as prop
                     // <PostItem savedPosts={savedPosts} />
                     this.state.isLoaded ? 
-                    <PostItem savedPosts={savedPosts} />
+                    // change the props passed to the PostItem to posts in the state
+                    <PostItem savedPosts={this.state.posts} />
                     : <Loader />
                 }
             </div>
