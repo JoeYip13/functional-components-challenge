7ce8d67e8add6d5894e8496357c231bf617538f2
JoeYip13 <>
2023-09-23 10:19:31 +0000

    feat(components): component lifecycles challenge

--- my-app/src/components/Content.js
+++ my-app/src/components/Content.js
@@ -2,41 +2,53 @@ import React, { Component } from 'react';
 import {savedPosts} from '../posts.json';
 import css from "./css/Content.module.css";
 import PostItem from './PostItem';
+import Loader from './Loader';
 
 export class Content extends Component {
     constructor(props) {
       super(props)
     
       this.state = {
+        isLoaded: false,
       }
     }
-  render() {
-    return (
-      <div className={css.Content}>
-        <div className={css.TitleBar}>
-            <h1>My Photos</h1>
+    componentDidMount() {
+        setTimeout(() => {
+            this.setState({
+                isLoaded: true,
+            })
+        }, 2000)
+    }
+    render() {
+        return (
+        <div className={css.Content}>
+            <div className={css.TitleBar}>
+                <h1>My Photos</h1>
+            </div>
+            <div className={css.SearchResults}>
+                
+                {/* Part 1 {savedPosts.map(post => {
+                    return (
+                        <div key={post.title} className={css.SearchItem}>
+                            <p>{post.title}</p>
+                            <p>{post.name}</p>
+                            <img src={post.image} alt="" />
+                            <p>{post.description}</p>
+                        </div>
+                    )
+                })} */}
+                
+                {
+                    // pass savedPost as prop
+                    // <PostItem savedPosts={savedPosts} />
+                    this.state.isLoaded ? 
+                    <PostItem savedPosts={savedPosts} />
+                    : <Loader />
+                }
+            </div>
         </div>
-        <div className={css.SearchResults}>
-            
-            {/* Part 1 {savedPosts.map(post => {
-                return (
-                    <div key={post.title} className={css.SearchItem}>
-                        <p>{post.title}</p>
-                        <p>{post.name}</p>
-                        <img src={post.image} alt="" />
-                        <p>{post.description}</p>
-                    </div>
-                )
-            })} */}
-            
-            {
-                // pass savedPost as prop
-                <PostItem savedPosts={savedPosts} />
-            }
-        </div>
-      </div>
-    )
-  }
+        )
+    }
 }
 
 export default Content
